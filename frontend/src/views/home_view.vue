be3788f8679e85d99cf494faad0658479166d757
Your GitHub Username <your-email@example.com>
2025-09-01 14:56:14 +0700

    navigation bar component to Home view

--- frontend/src/views/home_view.vue
+++ frontend/src/views/home_view.vue
@@ -1,3 +1,17 @@
+<script>
+import Navigation_bar from '@/components/layout/navigation_bar.vue'
+
+export default {
+  name: 'HomeView',
+  props: ['type'],
+  components: {
+    Navigation_bar,
+  },
+}
+</script>
+
 <template>
-  <div>ewdew</div>
+  <div>
+    <Navigation_bar />
+  </div>
 </template>
